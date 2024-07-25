99743fb52f15a9baa9d76af398bbccff62f58bac
IshantAhuja0 <ishantahuja2309@gmail.com>
2024-07-25 12:28:33 +0530

    day-1 commited

---
+++ DAY-1/Activities.js
@@ -0,0 +1,43 @@
+//activity-1
+var no=10;
+console.log(no);
+
+let st='hii!';
+console.log(st);
+
+//activity-2
+const bool=true;
+console.log(bool);
+
+//activity-3
+let number=50;
+console.log(typeof(number));
+
+let string='hii!';
+console.log(typeof(string));
+
+let array=[1,2,3,4,5]
+console.log(typeof(array));
+
+let boolean=true;
+console.log(typeof(bool));
+
+let obj={
+  fName:'ISHANT',
+  LnAME:'AHUJA',
+}
+console.log(typeof(obj));
+
+// activity-4
+let val=5;
+console.log(val);
+
+val=10
+console.log(val);
+
+// activity-5
+const j=5;
+console.log(j);
+
+j=10
+console.log(j);
